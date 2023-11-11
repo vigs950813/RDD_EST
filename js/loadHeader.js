152f43d23480864d29ebb0a373408a5b2125f460
vigs950813 <renovatio_bluesunset@hotmail.com>
2023-11-11 14:34:52 -0600

    prueba

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -1,8 +1,9 @@
-  // Utilizando Fetch para obtener el contenido del encabezado.html
-  fetch('dynamic/header.html')
-  .then(response => response.text())
-  .then(html => {
-      // Inserta el contenido del encabezado en el contenedor
-      document.getElementById('header-container').innerHTML = html;
-  })
-  .catch(error => console.error('Error loading header', error));
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('dynamic/header.html')
+       .then(response => response.text())
+       .then(html => {
+          document.getElementById('header-container').innerHTML = html;
+       })
+       .catch(error => console.error('Error loading header', error));
+ });
+ 
